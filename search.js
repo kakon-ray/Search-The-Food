d154c6e7475187bc408126ebef696cc8b0526549
Kakon Ray <thisiskakonray@gmail.com>
2022-03-14 16:28:27 +0600

    condition check

--- search.js
+++ search.js
@@ -76,7 +76,7 @@ function mailIdDetails(data) {
 // Enter Button Click search value show
 const searchFild = document.getElementById("search-fild");
 searchFild.addEventListener("keypress", function (event) {
-  if (event.keyCode == 13) {
+  if (event.key == "Enter") {
     document.getElementById("button").click();
   }
 });
